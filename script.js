f9be64c95355446ccab154242bd3f44b3de819fd
abawgus <abawgus@SHODAN.(none)>
2013-02-20 18:34:08 -0500

    more box functions

--- script.js
+++ script.js
@@ -15,33 +15,10 @@ $(document).ready(function(){
 	$( "#carousel" ).rcarousel({
 			auto: {enabled: true}
 		});
-
+});
 	// $('#temp1').click(function(){
 	// 	$('body').append($div);
 	// }).
-
- // $(document).keydown(function(key) {
- //        switch(parseInt(key.which,10)) {
- //    		case 65:
-	// 			$('img').animate({left: "-=10px"}, 'fast');
-	// 			break;
-	// 		case 83:
-	// 			$('img').animate({top: "+=10px"}, 'fast');
-	// 			break;
-	// 		case 87:
-	// 			$('img').animate({top: "-=10px"}, 'fast');
-	// 			break;
-	// 		case 68:
-	// 			$('img').animate({left: "+=10px"}, 'fast');
-	// 			break;
-	// 		case 70:
-	// 			$('body').append($('<div>empty</div>'));
-	// 			break;
-	// 		default:
-	// 			break;
-	// 	}
-	// });
-});
 
 function log(msg) {
     setTimeout(function() {
@@ -51,15 +28,30 @@ function log(msg) {
 
 var colors = new Array('red','blue','green');
 
-window.setInterval(function(){
-  var randTop = Math.round(400*Math.random());
-  var randLeft = Math.round(300*Math.random());
-  var randColor = Math.round(3*Math.random());
-  log(randColor);
-  $('#boxGarden').append($('<div class="sudden" style=" border-radius:10px;position:absolute; top:'+randTop+';left:'+randLeft+';height:50px; width:50px; background-color:'+colors[randColor]+';"></div>'));
-}, 500);
+function generateBoxes(){
+	var refreshIntervalId = window.setInterval(function(){
+	  var randTop = Math.round(400*Math.random());
+	  var randLeft = Math.round(300*Math.random());
+	  var randColor = Math.round(3*Math.random());
+	  log(randColor);
+	  $('#boxGarden').append($('<div class="sudden" style=" border-radius:10px;position:absolute; top:'+randTop+';left:'+randLeft+';height:50px; width:50px; background-color:'+colors[randColor]+';"></div>'));
+	}, 500);
+}
 
-window.setInterval(function(){
-  $( ".sudden" ).toggle( "explode" );
-  $(".sudden").remove();
-}, 2000);
+$( document ).keydown(function(key) {
+  switch(parseInt(key.which,10)){
+  	case 81: //letter q
+  	 	generateBoxes();
+  	 break
+  	case 68: //letter d
+  		$(".sudden").remove();
+	break
+	case 65: //letter a
+  		$( ".sudden" ).toggle( "shake" );
+  	break
+  	case 87:
+  		clearInterval(refreshIntervalId);
+  	default:
+  	break;  	
+  }
+});
